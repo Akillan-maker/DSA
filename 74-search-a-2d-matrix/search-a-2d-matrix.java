40c62df3b51a7bd2be9bcdfa91ad0bcc5f1041c8
Akil <akilhunter29@gmail.com>
2025-06-15 23:03:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (27.71%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,25 +1,33 @@
 class Solution {
 
-    boolean bs(int[] arr,int target){
+    // boolean bs(int[] arr,int target){
 
-        int low=0,high=arr.length-1;
+    //     int low=0,high=arr.length-1;
         
-        while(low<=high){
-            int mid=(low+high)/2;
+    //     while(low<=high){
+    //         int mid=(low+high)/2;
 
-            if(arr[mid]==target) return true;
-            if(arr[mid]>target) high=mid-1;
-            else low=mid+1;
-        }
-        return false;
-    }
+    //         if(arr[mid]==target) return true;
+    //         if(arr[mid]>target) high=mid-1;
+    //         else low=mid+1;
+    //     }
+    //     return false;
+    // }
     public boolean searchMatrix(int[][] matrix, int target) {
         
-        for(int i=0;i<matrix.length;i++){
-            if(matrix[i][0]<=target && matrix[i][matrix[i].length-1]>=target){
-                return bs(matrix[i],target);
-            }
-        }
+       int n=matrix.length,m=matrix[0].length;
+       int low=0,high=(n*m)-1;
+
+       while(low<=high){
+
+        int mid=(low+high)/2;
+        int row=mid/m;
+        int col=mid%m;
+
+        if(matrix[row][col]==target) return true;
+        else if(matrix[row][col]>target) high=mid-1;
+        else low=mid+1;
+       }
         return false;
     }
 }
